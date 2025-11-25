83ccabb048a080952b5173a3f6da89fca2b9ed4c
Neville Nazerane <live.neville@live.com>
2025-11-25 01:38:10 -0700

    bindings

---
+++ src/Profile.Website/Layout/MainLayout.razor.cs
@@ -0,0 +1,25 @@
+﻿using Profile.Website.Models;
+
+namespace Profile.Website.Layout
+{
+    public partial class MainLayout
+    {
+
+        private IEnumerable<SocialData> Socials = 
+            [
+                new()
+                {
+                    Class = "github",
+                    Icon = "bi-github",
+                    Url = "https://github.com/neville-nazerane"
+                },
+                 new()
+                {
+                    Class = "linkedin",
+                    Icon = "bi-linkedin",
+                    Url = "https://www.linkedin.com/in/neville-nazerane"
+                }
+            ];
+
+    }
+}
---
+++ src/Profile.Website/Models/SocialData.cs
@@ -0,0 +1,13 @@
+﻿namespace Profile.Website.Models
+{
+    public class SocialData
+    {
+
+        public required string Icon { get; set; }
+
+        public required string Class { get; set; }
+
+        public required string Url { get; set; }
+
+    }
+}

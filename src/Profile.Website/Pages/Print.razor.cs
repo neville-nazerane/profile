6f10ae0aecd63399275de86a1b932b2103e423b8
Neville Nazerane <live.neville@live.com>
2025-11-30 16:21:48 -0700

    print popup

--- src/Profile.Website/Pages/Print.razor.cs
+++ src/Profile.Website/Pages/Print.razor.cs
@@ -1,12 +1,15 @@
-﻿using Profile.Website.Models;
+﻿using Microsoft.JSInterop;
+using Profile.Website.Models;
 using Profile.Website.Services;
+using System.Runtime.CompilerServices;
 
 namespace Profile.Website.Pages
 {
-    public partial class Print(DataService dataService)
+    public partial class Print(DataService dataService, IJSRuntime js)
     {
 
         private readonly DataService _dataService = dataService;
+        private readonly IJSRuntime _js = js;
 
         private readonly AllContents contents = new();
 
@@ -14,6 +17,13 @@ namespace Profile.Website.Pages
         {
             await _dataService.PopulateAllAsync(contents);
             await base.OnInitializedAsync();
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+            if (!firstRender)
+                await _js.InvokeVoidAsync("window.print");
         }
 
     }
